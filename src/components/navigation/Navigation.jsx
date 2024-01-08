518907ffaac8556831f69429940581135d13b2bd
ingelos <mailvaninge@gmail.com>
2024-01-08 14:38:17 +0100

    navigation component setup

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,6 +1,29 @@
 import './Navigation.css'
+import {NavLink} from "react-router-dom";
 
 function Navigation() {
+    return (
+        <>
+            <nav>
+                <div className='nav-container'>
+                    <ul className='navigation-links'>
+                        <li>
+                            <NavLink className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                                     to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                                     to='/my-books'>MyBooks</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                                     to='/browse'>Browse</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navigation;
