0795cba6260853e2f6df5771936451b8b38140da
ingelos <mailvaninge@gmail.com>
2024-01-10 16:27:14 +0100

    register page css added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,35 +1,31 @@
-import {Link} from "react-router-dom";
-import Caret from "../../assets/icons/caret-right.svg";
-
+import './Register.css'
 
 function Register() {
-    return(
+    return (
         <>
             <div className='register-page outer-container'>
                 <div className='register-page inner-container'>
                     <div className='inner-content-container'>
                         <form className='register-form' onSubmit={''}>
-                            <h3 className='register-title'>Create account</h3>
-                            <p className='register-subtitle'>Personal information</p>
-                            <div className='name-field'>
-                            <label htmlFor='register-first-name'>First name*</label>
-                            <input type='text'
-                                   id='first-name-field'
-                                   name='first-name'
-                                   value={''}
-                                   onChange={''}
-                                   required
-                            />
-                            <label htmlFor='register-last-name'>Last name*</label>
-                            <input type='text'
-                                   id='last-name-field'
-                                   name='last-name'
-                                   value={''}
-                                   onChange={''}
-                                   required
-                            />
-                            </div>
-                            <p className='register-information'>Login information</p>
+                            <h2 className='register-title'>Create account</h2>
+                            <h3 className='register-subtitle'>Personal information</h3>
+                            <div className='personal-information-field'>
+                                <label htmlFor='register-first-name'>First name*</label>
+                                <input type='text'
+                                       id='first-name-field'
+                                       name='first-name'
+                                       value={''}
+                                       onChange={''}
+                                       required
+                                />
+                                <label htmlFor='register-last-name'>Last name*</label>
+                                <input type='text'
+                                       id='last-name-field'
+                                       name='last-name'
+                                       value={''}
+                                       onChange={''}
+                                       required
+                                />
                             <label htmlFor='login-email'>E-mail*</label>
                             <input type='email'
                                    id='email-field'
@@ -38,27 +34,32 @@ function Register() {
                                    onChange={''}
                                    required
                             />
-                            <div className='password-field'>
-                            <label htmlFor='login-password'>Password*</label>
-                            <input type='password'
-                                   id='password-field'
-                                   name='password'
-                                   value={''}
-                                   onChange={''}
-                                   required
-                            />
-
-                                   <label htmlFor='login-password-confirm'>Confirm password*</label>
-                            <input type='password'
-                                   id='password-confirm-field'
-                                   name='password'
-                                   value={''}
-                                   onChange={''}
-                                   required
-                            />
                             </div>
-                            <p>(A password requires a minimum of 8 characters)</p>
-                        <button type='submit' className='register-button'>Create account</button>
+                            <div className='password-field'>
+                                <h3 className='register-subtitle'>Choose password</h3>
+                                <label htmlFor='login-password'>Password**</label>
+                                <input type='password'
+                                       id='password-field'
+                                       name='password'
+                                       value={''}
+                                       onChange={''}
+                                       required
+                                />
+                                <label htmlFor='login-password-confirm'>Confirm password*</label>
+                                <input type='password'
+                                       id='password-confirm-field'
+                                       name='password'
+                                       value={''}
+                                       onChange={''}
+                                       required
+                                />
+                            </div>
+                            <span className='notes'>
+                                <p>* required</p>
+                                <p id='required'>** a password requires a minimum of 8 characters</p>
+                            </span>
+                            <button type='submit' className='register-button'>Create account</button>
+
                         </form>
                     </div>
                 </div>
