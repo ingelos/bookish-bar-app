2671b2fb841979feece286bab19589f143fc4174
ingelos <mailvaninge@gmail.com>
2024-01-28 19:53:53 +0100

    added function BookCard Component

--- src/components/bookCard/BookCard.jsx
+++ src/components/bookCard/BookCard.jsx
@@ -1,13 +1,18 @@
 import {Link} from "react-router-dom";
+import './BookCard.css'
 
-function BookCard({ cover, id, title, author, year}) {
+function BookCard({cover, id, title, author, year}) {
     return (
-        <li key={`${title}-${author}`}>
-            <img src={cover} alt={`cover of ${title}`} />
-            <h2><Link to={`/books/${id}`}>{title}</Link></h2>
-            <h3>{author}</h3>
-            <p>First published in {year}</p>
-        </li>
+        <div className='book-card'>
+            <li className='book-card-list' key={`${title}-${author}`}>
+                <img src={cover} alt={`cover of ${title}`} className='book-cover'/>
+                <div className='book-card-info'>
+                    <h3><Link to={`/books/${id}`} className='book-link'>{title}</Link></h3>
+                    <h4>{author}</h4>
+                    <p>First published in {year}</p>
+                </div>
+            </li>
+        </div>
     )
 }
 
--- src/pages/browseRomance/BrowseRomance.jsx
+++ src/pages/browseRomance/BrowseRomance.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import BookCard from "../../components/bookCard/BookCard.jsx";
+import './BrowseRomance.css'
 
 
 function BrowseRomance() {
@@ -25,7 +26,7 @@ function BrowseRomance() {
 
             } catch (e) {
                 if (axios.isCancel(e)) {
-                console.error('Request is cancelled');
+                    console.error('Request is cancelled');
                 } else {
                     console.error(e);
                     setError(true);
@@ -37,46 +38,41 @@ function BrowseRomance() {
 
         fetchRomance();
 
-        return function cleanup()  {
+        return function cleanup() {
             controller.abort();
         }
 
     }, []);
 
 
-
     return (
 
         <section className='romance-section outer-container'>
             <div className='romance-section inner-container'>
-                <div className='book-card'>
-                    <div className='subject-header'>
+                <div className='subject-container'>
                     <h2 className='subject-header-title'>Romance</h2>
-                    </div>
+                </div>
+                <article className='book-card-container'>
+
                     {Object.keys(books).length > 0 &&
                         <>
                             <ul>
-                            {books.map((book) => {
-                                return (
-                                    // <li key={book.key}>
-                                    //     <img src={''}/>
-                                    //     <h2>{book.title}</h2>
-                                    //     <h3>{book.authors[0].name}</h3>
-                                    //     <p>First published in {book.first_publish_year}</p>
-                                    // </li>
-                                    <BookCard
-                                        key={`${book.title}-${book.authors[0].name}`}
-                                        title={book.title}
-                                        author={book.authors[0].name}
-                                        year={book.first_publish_year}
-                                    />
-                                )
-                            })}
+                                {books.map((book) => {
+                                    return (
+                                        <BookCard
+                                            id={book.id}
+                                            key={`${book.title}-${book.authors[0].name}-${book.key}`}
+                                            title={book.title}
+                                            author={book.authors[0].name}
+                                            year={book.first_publish_year}
+                                        />
+                                    )
+                                })}
                             </ul>
                         </>
                     }
                     {loading && <p>Loading...</p>}
-                </div>
+                </article>
             </div>
         </section>
     )
